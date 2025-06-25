efcbf048a91b905e298fb24f4552698575a6adc6
alvaro-gh <aaraujo@pm.me>
2025-06-24 22:55:16 -0300

    Remove init

--- client.py
+++ client.py
@@ -1,53 +1,56 @@
 import asyncio
-import os
-from contextlib import AsyncExitStack
 
-from openai import AsyncOpenAI
 from agents import Agent, Runner
 from agents.mcp import MCPServerStreamableHttp
 from agents.model_settings import ModelSettings
 
+
 class MCPClient:
-  def __init__(self):
-    self.ai_client = AsyncOpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+    """Client MCP Class."""
 
-  async def process_query(self, query: str) -> str:
-    async with MCPServerStreamableHttp(name='weather', params={'url': 'http://localhost:8000/mcp'}) as server:
-      agent = Agent(
-        name='Assistant',
-        instructions="""
-          Utiliza las herramientas para proveer respuestas acerca del estado del tiempo en una ciudad de Argentina.
-          Si la herramienta no devuelve información entonces disculpate con un mensaje corto.
-          Si la herramienta devuelve alguna característica relacionada al cielo entonces debes responder cómo se encuentra el cielo.
-          Si la herramienta devuelve que ademas hay algún tipo de precipitación debes separar la respuesta entre el estado del cielo y las precipitaciones.
+    async def process_query(self, query: str) -> str:
+        """Process incoming queries to this client."""
+        async with MCPServerStreamableHttp(
+            name="weather", params={"url": "http://localhost:8000/mcp"}
+        ) as server:
+            agent = Agent(
+                name="Assistant",
+                instructions="""
+          Use tools to provide answers about the weather in Argentina cities.
+          If the tool does not return information then make a short apology.
+          The tool returns one or few words so adapt your answer to a short sentence.
+          The tool returns words in spanish so translate your final answer to english.
           """,
-        mcp_servers=[server],
-        model_settings=ModelSettings(tool_choice='required')
-      )
+                mcp_servers=[server],
+                model_settings=ModelSettings(tool_choice="required"),
+            )
 
-      response = await Runner.run(starting_agent=agent, input=query)
-      return response.final_output
+            response = await Runner.run(starting_agent=agent, input=query)
+            return response.final_output
 
-  async def chat_loop(self):
-    print("\n---Cliente MCP---")
-    print("Pregunta por el clima en una ciudad. Escribi exit para salir.")
+    async def chat_loop(self) -> None:
+        """Chat with user on a loop."""
+        print("---MCP Client---")  # noqa: T201
+        print(  # noqa: T201
+            "Ask about the weather in a city (Argentina). Type exit to finish the chat."
+        )
 
-    while True:
-      try:
-        query = input("\nQuery: ").strip()
+        while True:
+            try:
+                query = input("\nQuery: ").strip()
+                if query.lower() == "exit":
+                    break
+                response = await self.process_query(query)
+                print("\n" + response)  # noqa: T201
+            except Exception as e:
+                print(f"\nError: {e}")  # noqa: T201
 
-        if query.lower() == "exit":
-          break
 
-        response = await self.process_query(query)
-        print("\n" + response)
+async def main() -> None:
+    """Run the client."""
+    client = MCPClient()
+    await client.chat_loop()
 
-      except Exception as e:
-        print(f"\nError: {str(e)}")
-
-async def main():
-  client = MCPClient()
-  await client.chat_loop()
 
 if __name__ == "__main__":
-  asyncio.run(main())
+    asyncio.run(main())
--- server.py
+++ server.py
@@ -1,15 +1,18 @@
 import pymongo
 from mcp.server.fastmcp import FastMCP
 
-mcp = FastMCP(name='weather', stateless_http=True, host='127.0.0.1', port=8000)
+mcp = FastMCP(name="weather", stateless_http=True, host="127.0.0.1", port=8000)
+
 
 @mcp.tool()
 async def get_weather(city: str) -> str:
-  client = pymongo.MongoClient('localhost', 27017)
-  database = client.smn
-  collection = database.weather
-  r = collection.find_one({'name': city})
-  return r['weather']['description']
+    """Get weather information from MongoDB."""
+    client = pymongo.MongoClient("localhost", 27017)
+    database = client.smn
+    collection = database.weather
+    r = collection.find_one({"name": city})
+    return r["weather"]["description"]  # type: ignore
 
-if __name__ == '__main__':
-  mcp.run(transport='streamable-http')
+
+if __name__ == "__main__":
+    mcp.run(transport="streamable-http")
